09fc949cf939a7bdf3c029947ac922362c19d66f
QuaRang <dbduddnd1225@gmail.com>
2025-04-08 15:58:20 +0900

    feta: SegmentControl 데이터 바인딩 (#6)
    - ViewModel에서 Input/Ouput protocol 채택
    - VM에서 청사진에 따른 기능(세그먼트에 대한 입력 및 출력 데이터)를 정의
    
    - transform에서 index값에 따른 네트워크 API 실행
    - fetchProducts에서 데이터를 받으면 prducts를 subject에 방출
    - 결론적으로 VC에서 해당 subject를 구독할 때 segment 변경사항에 따라 이벤트를 방출하는 단방향 로직 사용

--- KioskProject/Presentation/Sources/ViewController/MainViewController.swift
+++ KioskProject/Presentation/Sources/ViewController/MainViewController.swift
@@ -6,14 +6,19 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+
+import SnapKit
 
 final class MainViewController: UIViewController {
     
     private let viewModel: MainViewModel
+    private let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        bindViewModel()
         view.backgroundColor = .systemBackground
     }
     
@@ -25,5 +30,25 @@ final class MainViewController: UIViewController {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func bindViewModel(){
+        let segment = UISegmentedControl(items: ["SmartPhone","Laptops","tablets"])
+        segment.selectedSegmentIndex = 0
+        view.addSubview(segment)
+        
+        segment.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+
+        let input = MainViewModel.Input(selectedIndex: segment.rx.selectedSegmentIndex.asObservable())
+        let output = viewModel.transform(input: input)
+        
+        output.setInfo
+            .observe(on: MainScheduler.instance)
+            .bind { products in
+                print("데이터 업데이트: \(products)")
+            }
+            .disposed(by: disposeBag)
     }
 }
--- KioskProject/Presentation/Sources/ViewModel/MainViewModel.swift
+++ KioskProject/Presentation/Sources/ViewModel/MainViewModel.swift
@@ -6,25 +6,64 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
 
-final class MainViewModel{
+protocol ViewModel {
+    associatedtype Input
+    associatedtype Output
+    
+    var disposeBag: DisposeBag { get set }
+    
+    func transform(input: Input) -> Output
+}
+
+final class MainViewModel: ViewModel{
+    
+    
+    struct Input {
+        let selectedIndex: Observable<Int>
+    }
+    
+    struct Output {
+        let setInfo: BehaviorSubject<[Product]>
+    }
+    
+    var disposeBag = DisposeBag()
+    
+    private let products = BehaviorSubject<[Product]>(value: [])
     
     private let useCase: KioskUseCaseInterface
     
     init(useCase: KioskUseCaseInterface) {
         self.useCase = useCase
-        
-        fetchProducts(type: .tablets)
     }
     
     func fetchProducts(type: ProductType) {
         Task{
             do{
-                let result = try await useCase.fetchProducts(type: type)
-                print(result)
+                let products = try await useCase.fetchProducts(type: type)
+                self.products.onNext(products)
             }catch {
                 print(error.localizedDescription)
             }
         }
     }
+    
+    func transform(input: Input) -> Output {
+        input.selectedIndex
+            .subscribe(onNext: { [weak self] index in
+                guard let self else { return }
+                switch index {
+                case 0: self.fetchProducts(type: .smartphones)
+                case 1: self.fetchProducts(type: .laptops)
+                case 2: self.fetchProducts(type: .tablets)
+                default: break
+                }
+            })
+            .disposed(by: disposeBag)
+        
+        return Output(setInfo: products)
+    }
 }
+
