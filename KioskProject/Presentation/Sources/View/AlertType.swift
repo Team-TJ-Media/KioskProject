ff53743c4cebb606f594ef4b1e739bd74835e146
RaeBaek <bmp117@naver.com>
2025-04-09 14:17:03 +0900

    feat: AlertType 정의 및 UIViewController showAlert 메서드 구현 (#18)
    - AlertType enum 추가
      - emptyCart: 장바구니가 비어있는 경우
      - confirmCancel: 주문 취소 확인용
      - confirmOrder(Int): 결제 금액을 포함한 주문 확인용
    
    - UIViewController extension에 showAlert 메서드 구현
      - AlertType 기반으로 title, message 구성
      - handler를 통해 버튼 클릭 이벤트 처리 가능

---
+++ KioskProject/Presentation/Sources/View/AlertType.swift
@@ -0,0 +1,36 @@
+//
+//  AlertType.swift
+//  KioskProject
+//
+//  Created by 백래훈 on 4/9/25.
+//
+
+import Foundation
+
+enum AlertType {
+    case emptyCart
+    case confirmCancel
+    case confirmOrder(Int)
+    
+    var title: String {
+        switch self {
+        case .emptyCart:
+            return "주문 실패"
+        case .confirmCancel:
+            return "주문 취소"
+        case .confirmOrder:
+            return "주문 확인"
+        }
+    }
+    
+    var message: String {
+        switch self {
+        case .emptyCart:
+            return "장바구니가 비어있습니다.\n먼저 상품을 장바구니에 담아주세요!"
+        case .confirmCancel:
+            return "장바구니 내역이 삭제됩니다.\n정말 취소하시겠습니까?"
+        case .confirmOrder(let price):
+            return "총 결제 금액은 ₩\(price)입니다. 결제하시겠습니까?"
+        }
+    }
+}
