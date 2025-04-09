f47150356048fd25ee93c71bdeeaa7991f4a49c7
QuaRang <dbduddnd1225@gmail.com>
2025-04-09 11:36:03 +0900

    feat: UI 개발 브랜치 적용 후 바인딩 작업(#6)
    - 기존에 테스트로 만들어 둔 TestMainview 삭제하고 기존에 있던 MainView에 대체
    - 변수명 및 메서드명 변경(구현된 UI에 맞게)
    - 이미지 뒷배경 색상 흰색으로 수정

--- KioskProject/Presentation/Sources/ViewController/MainViewController.swift
+++ KioskProject/Presentation/Sources/ViewController/MainViewController.swift
@@ -29,9 +29,6 @@ final class MainViewController: UIViewController {
         super.viewDidLoad()
         bindViewModel()
         view.backgroundColor = .systemBackground
-        
-        mainView.productCollectionView.delegate = self
-        mainView.productCollectionView.dataSource = self
     }
     
     init(DIContainer: KioskDIContainerInterface) {
@@ -47,18 +44,18 @@ final class MainViewController: UIViewController {
     private func bindViewModel(){
         
         let input = MainViewModel.Input(
-            selectedIndex: mainView.segment.rx.selectedSegmentIndex.asObservable(),
-            selectedCell: mainView.collectionView.rx.itemSelected.map{$0.row}.asObservable()
+            selectedIndex: mainView.categoryView.rx.selectedSegmentIndex.asObservable(),
+            selectedCell: mainView.productCollectionView.rx.itemSelected.map{$0.row}.asObservable()
         )
         let output = viewModel.transform(input: input)
         
         output.setInfo
             .observe(on: MainScheduler.instance)
-            .bind(to: mainView.collectionView.rx.items(
-                    cellIdentifier: ProductCell.identifier,
-                    cellType: ProductCell.self)
+            .bind(to: mainView.productCollectionView.rx.items(
+                    cellIdentifier: ProductCollectionViewCell.identifier,
+                    cellType: ProductCollectionViewCell.self)
             ){ _, product, cell in
-                cell.configure(with: product)
+                cell.configure(product: product)
             }
             .disposed(by: disposeBag)
         
@@ -68,66 +65,6 @@ final class MainViewController: UIViewController {
                 print("데이터: \(cell)")
             }
             .disposed(by: disposeBag)
-    }
-}
-
-class ProductCell: UICollectionViewCell {
-    static let identifier = "ProductCell"
-    
-    private let nameLabel = UILabel()
-
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        contentView.addSubview(nameLabel)
-        nameLabel.frame = contentView.bounds
-        nameLabel.textAlignment = .center
-    }
-
-    func configure(with product: Product) {
-        nameLabel.text = product.title
-    }
-
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-}
-
-final class TestMainView: UIView {
-    let segment: UISegmentedControl = {
-        let segment = UISegmentedControl(items: ["SmartPhone","Laptops","tablets"])
-        segment.selectedSegmentIndex = 0
-        return segment
-    }()
-    
-    let collectionView: UICollectionView = {
-        let layout = UICollectionViewFlowLayout()
-        layout.itemSize = CGSize(width: 120, height: 60)
-        layout.scrollDirection = .vertical
-        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        cv.register(ProductCell.self, forCellWithReuseIdentifier: ProductCell.identifier)
-        return cv
-    }()
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setupViews()
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError()
-    }
-    
-    private func setupViews() {
-        addSubview(segment)
-        addSubview(collectionView)
-        segment.snp.makeConstraints { make in
-            make.top.equalTo(safeAreaLayoutGuide.snp.top).offset(16)
-            make.leading.trailing.equalToSuperview().inset(20)
-        }
-        collectionView.snp.makeConstraints { make in
-            make.top.equalTo(segment.snp.bottom).offset(16)
-            make.leading.trailing.bottom.equalToSuperview()
-        }
     }
 }
 
