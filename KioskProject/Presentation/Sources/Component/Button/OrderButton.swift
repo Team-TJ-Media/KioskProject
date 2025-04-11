262666cec9e089d691b7b7fbb425207d5001d2e5
QuaRang <dbduddnd1225@gmail.com>
2025-04-11 10:43:13 +0900

    refactor: HotLabel 추기 (#21)
    - CollectionView 셀 오른쪽에 HotLabel 배치
    - 화이트 모드에서 주문하기 버튼 border가 안보이는 문제 해결

--- KioskProject/Presentation/Sources/Component/Button/OrderButton.swift
+++ KioskProject/Presentation/Sources/Component/Button/OrderButton.swift
@@ -43,18 +43,21 @@ class OrderButton: UIButton {
         setTitleColor(orderStatus.titleColor(), for: .normal)
         layer.cornerRadius = 8
         
+        
+        
+        snp.makeConstraints {
+            $0.height.equalTo(50)
+        }
+    }
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
         if orderStatus == .confirm {
             layer.borderColor = UIColor.label.cgColor
             layer.borderWidth = 2
         } else {
             backgroundColor = .label
         }
-        
-        snp.makeConstraints {
-            $0.height.equalTo(50)
-        }
     }
-    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")   }
 }
